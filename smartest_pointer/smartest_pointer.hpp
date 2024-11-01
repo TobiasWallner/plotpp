1666107640a385b5f4bf7bf503524a4c27d1e69f
Tobias Wallner <tobias.wallner1@gmx.net>
2024-11-01 12:47:28 +0100

    simplified smartest pointer and removed the need of a std::variant

--- smartest_pointer/smartest_pointer.hpp
+++ smartest_pointer/smartest_pointer.hpp
@@ -1,53 +1,51 @@
-#include <variant>
+#pragma once
 
-template<typename T>
-class smartest_pointer;
+#include <memory>
+
+/*
+	A pointer type that takes posession of the memory if moved into the smartest_pointer 
+	or holds a non-owning reference/pointer otherwise
+*/
 
 template<typename T>
 class smartest_pointer {
 public:
-    smartest_pointer(T& ref) : pointer_(&ref) {}
+    inline smartest_pointer(T& ref) 
+		: _mem()
+		, _ptr(&ref) {}
 	
-	//TODO: small object optimisation for up to 64 byte ?
-	smartest_pointer(T&& ref) : pointer_(std::make_unique<T>(std::move(ref))) {}
+	inline smartest_pointer(T&& ref) 
+		: _mem(std::make_shared<T>(std::move(ref)))
+		, _ptr(this->_mem.get()) {}
 	
 	// Constructor from raw pointer
-    smartest_pointer(T* ptr) : pointer_(ptr) {}
+    inline smartest_pointer(T* ptr)
+		: _mem()
+		, _ptr(ptr) {}
 
     // Constructor from unique_ptr
-    smartest_pointer(std::unique_ptr<T>&& ptr) : pointer_(std::shared_ptr<T>(std::move(ptr))) {}
+    inline smartest_pointer(std::unique_ptr<T>&& ptr) 
+		: _mem(std::shared_ptr<T>(std::move(ptr)))
+		, _ptr(this->_mem.get()) {}
 
     // Constructor from shared_ptr
-    smartest_pointer(std::shared_ptr<T> ptr) : pointer_(std::move(ptr)) {}
+    inline smartest_pointer(std::shared_ptr<T> ptr)
+		: _mem(std::move(ptr))
+		, _ptr(this->_mem.get()) {}
 
-	smartest_pointer(const smartest_pointer&) = default;
-	smartest_pointer& operator=(const smartest_pointer&) = default;
+	inline smartest_pointer(const smartest_pointer&) = default;
+	inline smartest_pointer& operator=(const smartest_pointer&) = default;
 
-    smartest_pointer(smartest_pointer&&) = default;
-    smartest_pointer& operator=(smartest_pointer&&) = default;
+    inline smartest_pointer(smartest_pointer&&) = default;
+    inline smartest_pointer& operator=(smartest_pointer&&) = default;
 
     // Dereference operator
-    T& operator*() const {return *getPointer();}
+    inline auto& operator*() const {return *_ptr;}
 
     // Arrow operator
-    T* operator->() const {return getPointer();}
-
-    
+    inline auto* operator->() const {return _ptr;}
 
 private:
-
-	// TODO: Remove the variant by making a template specialisation for each element
-    std::variant<T*, std::shared_ptr<T>> pointer_;
-
-    // Helper function to retrieve the raw pointer from the variant
-	T* getPointer() const {
-        switch (pointer_.index()) {
-            case 0:  // T* (raw pointer)
-                return *std::get_if<T*>(&pointer_);
-            case 1:  // std::unique_ptr<T>
-                return std::get_if<std::shared_ptr<T>>(&pointer_)->get();
-            default:
-                return nullptr;  // Should never happen if the variant is properly initialized
-        }
-    }
+	inline std::shared_ptr<T> _mem;
+	inline T* _ptr;
 };
