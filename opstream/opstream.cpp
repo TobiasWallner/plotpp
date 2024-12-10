f640320846fd266ae0369927d03b12c6812c6db3
Tobias Wallner <tobias.wallner1@gmx.net>
2024-12-11 00:08:21 +0100

    make the buffer not static but dynamically allocated on the heap. added move semantic tothe stream buffer and the stream

--- opstream/opstream.cpp
+++ opstream/opstream.cpp
@@ -1,39 +1,91 @@
 #include "opstream.hpp"
 #include <iostream>
+#include <exception>
 
 //
 // Stream Buffer Functions
 //
 
 opstreambuf::opstreambuf(char const * process) {
+	this->open(process);
+}
+
+opstreambuf::opstreambuf(opstreambuf&& other){
+	this->buffer = std::move(other.buffer);
+	this->fprocess = other.fprocess;
+	other.fprocess = nullptr;	
+}
+
+opstreambuf& opstreambuf::operator=(opstreambuf&& other){
+	this->close();
+	if(this != &other){
+		this->buffer = std::move(other.buffer);
+		this->fprocess = other.fprocess;
+		other.fprocess = nullptr;	
+	}
+	return *this;
+}
+
+opstreambuf::~opstreambuf(){
+	this->close();
+}
+
+void opstreambuf::open(char const* process){
+	// close an existing open stream
+	if(this->is_open()) this->close();
+	
+	// allocating the buffer
+	constexpr size_t bufferSize = 128;
+	this->buffer = std::make_unique<char[]>(bufferSize);
+	this->std::streambuf::setp(buffer.get(), buffer.get() + bufferSize);
+	
+	// open the FILE pointer
 	#ifdef WIN32
 	fprocess = _popen(process, "w");
 	#else
 	fprocess = popen(process, "w");
 	#endif
+
 	if(process == nullptr){
 		std::string s;
-		s += "failed: process = popen(process, \"w\");"; 
-
+		s = "Could not open the pipe stream with the command: ";
+		s += process;
+		throw std::runtime_error(s);
 	}
-	setp(buffer, buffer + sizeof(buffer) - 1); // Reserve space for a null terminator
 }
 
-opstreambuf::~opstreambuf(){
+void opstreambuf::close(){
 	this->sync();
-	#ifdef WIN32
-	int status = _pclose(fprocess);
-	#else
-	int status = pclose(fprocess);
-	#endif	
-	if (status != 0) std::cerr << "Could not close the pipe stream" << std::endl;;
+	
+	// deallocate the buffer
+	if(this->buffer){
+		this->buffer.reset();
+	}
+	
+	// close the pipe stream
+	if(this->fprocess != nullptr){		
+		#ifdef WIN32
+		int status = _pclose(this->fprocess);
+		#else
+		int status = pclose(this->fprocess);
+		#endif	
+		
+		if (status != 0) 
+			throw std::runtime_error("Could not close the pipe stream");
+		else 
+			this->fprocess = nullptr;
+	}
+}
+
+bool opstreambuf::is_open(){
+	return this->fprocess != nullptr;
 }
 
 int opstreambuf::sync() {
 	std::streamsize num = pptr() - pbase();
 	if (num > 0) {
-		size_t r = fwrite(pbase(), sizeof(char), num, fprocess);
-		pbump(-num); 
+		size_t r = fwrite(this->std::streambuf::pbase(), sizeof(char), num, fprocess);
+		this->std::streambuf::pbump(-num); 
 		if(r != num){
 			return -1; // error
 		}
@@ -44,11 +96,38 @@ int opstreambuf::sync() {
 int opstreambuf::overflow(int c) {
 	int r = this->sync();
 	if(r != 0) return -1; // error
-	buffer[0] = static_cast<char>(c);
-	pbump(1);
+	this->buffer[0] = static_cast<char>(c);
+	this->std::streambuf::pbump(1);
 	return 0;
 }
+
+// ----------------------------------------------------------
+//							opstream
+// ----------------------------------------------------------
 
 opstream::opstream(char const * process) 
 	: std::ostream(&buf)
 	, buf(process){}
+	
+opstream::opstream(opstream&& other){
+	this->buf = std::move(other.buf);
+}
+
+opstream& opstream::operator=(opstream&& other){
+	if(this != &other){
+		this->buf = std::move(other.buf);
+	}
+	return *this;
+}	
+
+void opstream::open(char const* process){
+	this->buf.open(process);
+}
+	
+void opstream::close(){
+	this->buf.close();
+}
+
+bool opstream::is_open(){
+	return this->buf.is_open();
+}
--- opstream/opstream.hpp
+++ opstream/opstream.hpp
@@ -1,16 +1,29 @@
 #pragma once
 #include <cstdio>
 #include <ostream>
+#include <memory>
 
 // output pipe stream buffer
 class opstreambuf : public std::streambuf {
 public:
+	opstreambuf() = default;
     opstreambuf(char const * process);
+	
+	opstreambuf(const opstreambuf&) = delete;
+	opstreambuf(opstreambuf&& other);
+	
+	opstreambuf& operator=(const opstreambuf&) = delete;
+	opstreambuf& operator=(opstreambuf&& other);
+	
 	virtual ~opstreambuf();
+
+	void open(char const* process);
+	void close();
+	bool is_open();
 
 protected:
 
-    char buffer[1024];
+    std::unique_ptr<char[]> buffer;
 	FILE* fprocess = nullptr;
 
 	// specify what should be done to flush the buffer (aka. synchronise the buffer with the device)
@@ -20,7 +33,18 @@ protected:
 
 class opstream : public std::ostream {
 public:
+	opstream() = default;
 	opstream(char const * process);
+	
+	opstream(const opstream&) = delete;
+	opstream(opstream&& other);
+	
+	opstream& operator=(const opstream&) = delete;
+	opstream& operator=(opstream&& other);
+	
+	void open(char const* process);
+	void close();
+	bool is_open();
 	
 protected:
 	opstreambuf buf;
