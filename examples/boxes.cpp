11a6fea7fb9a1818f4dde05f83d0c62e29f16c12
Tobias Wallner <tobias.wallner1@gmx.net>
2025-03-02 14:15:44 +0100

    plotting to svg images

--- examples/boxes.cpp
+++ examples/boxes.cpp
@@ -4,22 +4,24 @@ int main() {
 	using namespace plotpp;
 	
 	int xdata[] = {0, 1, 2, 3, 4, 5, 6};
-	float ydata[] = {5.0, 6.5, 7.8, 6.0, 7.0};
+	float ydata[] = {1.0, 5.5, 7.8, 9.0, 6.0};
 	
-	float y2data[] = {3.0, 6.5, 9.8, 10.0, 2.0};
+	float y2data[] = {1.0, 4.5, 7.8, 9.0, 2.0};
 	int x2data[] = {3, 4, 5, 7, 8};
 	
 	Figure fig("Boxes");
 	fig.add(boxes(&xdata, &ydata).label("boxes1"));
 	fig.add(boxes(&x2data, &y2data).label("boxes2").relativeBoxWidth(false));
 	fig.show();
-	fig.save("script.gp");
+	fig.save("boxes-plot.svg");
 
 	
 	Figure fig2("Boxes with text");
+	fig2.yLim(0, 10);
 	fig2.xtics({"house", "bottel", "basket", "number", "apple"});
-	fig2.add(boxes(/*&xdata, */&ydata).label("boxes with names"));
+	fig2.add(boxes(&ydata).label("boxes with names"));
 	fig2.show();
+	fig2.save("boxes-with-named-xtics-plot.svg");
 
     return 0;
 }
